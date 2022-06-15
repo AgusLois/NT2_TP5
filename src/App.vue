9e841f99d1b5157eda8bef9afbeb7597b8c644f5
AgusLois <Agusslois98@gmail.com>
2022-06-14 23:16:43 -0300

    Agustina Lois

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+  import Home from './componentes/Home.vue'
+  export default {
+    name: 'App',
+    components: {
+      Home
+    }
+  }
+</script>
+
+<style>
+  
+  body {
+    background: #232323;
+    margin: 0;
+    font-family: "Montserrat", "Avenir";
+  }
+  h1 {
+    font-weight: normal;
+    line-height: 1.1;
+    padding: 20px 0;
+  }
+</style>
